71afa46a10c4638079f52faf3fb69b76ba5c02ea
Lukas Flad <fladlukas@icloud.com>
2023-10-19 15:24:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,49 @@ function splitAndDisplay() {
         document.getElementById('output').innerHTML = output.trim();  // Remove trailing space
     });
 }
+
+
+function splitAndDisplay() {
+    const inputText = document.getElementById('inputText').value;
+    fetch('/split-text', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ text: inputText })
+    })
+    .then(response => response.json())
+    .then(data => {
+        let output = '';
+        let colorToggle = true;  // Always start with blue
+
+        const splitWords = data.splitWords;
+
+        for (const word of splitWords) {
+            const syllables = word.split('-');
+            for (let i = 0; i < syllables.length; i++) {
+                // If the syllable is a punctuation mark, make it blue
+                if (/[.,!?;:]/.test(syllables[i])) {
+                    output += `<span class="blue">${syllables[i]}</span>`;
+                } else {
+                    const colorClass = colorToggle ? 'blue' : 'red';
+                    output += `<span class="${colorClass}">${syllables[i]}</span>`;
+                    // Toggle the color if there's another syllable after the current one
+                    if (i < syllables.length - 1) {
+                        colorToggle = !colorToggle;
+                    }
+                }
+            }
+
+            // Reset color toggle to always start the next word with blue
+            colorToggle = true;
+
+            output += ' ';
+        }
+
+        document.getElementById('output').innerHTML = output;
+    });
+}
+
+
+
