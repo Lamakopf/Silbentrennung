26ad021295932aca871773d6272c5a44d17e9dd8
Lukas Flad <fladlukas@icloud.com>
2023-10-19 16:38:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,15 @@
-
-    Hyphenopoly.config({
-        require: {
-            "de": "FORCEHYPHENOPOLY"
-        },
-        hyphen: "•",
-        minWordLength: 3
-    });
-
-
-var Separator = "•";
-
-var Hyphenopoly = {
-    "require": {
+Hyphenopoly.config({
+    require: {
         "de": "FORCEHYPHENOPOLY"
     },
-    "setup": {
-        "exceptions": {
-            "global": "Deutsch-ler-nen-de"
-        },
-        "selectors": {
-            ".hyphenate": {
-                "hyphen": Separator,
-                "minWordLength": 3
-            }
-        }
-    }
-}
+    exceptions: {
+        "de": "Deutsch-lern-ende"
+    },
+    hyphen: "•",
+    minWordLength: 3
+});
 
-function splitAndDisplay() {
-    const inputText = document.getElementById('inputText').value;
-    const colorScheme = document.getElementById('colorScheme').value;
-
-    // Define color mappings
-    const colorMappings = {
-        "highContrast": {
-            "blue": "#0000FF",
-            "red": "#FF0000"
-        },
-        "classicColors": {
-            "blue": "#005996",
-            "red": "#96000e"
-        }
-    };
-
-    // Get the selected colors
-    const selectedColors = colorMappings[colorScheme];
-
-    Hyphenopoly.hyphenators["de"].then((hyphenator) => {
-        let output = '';
-        let colorToggle = true;  // Always start with blue
-
-        const splitWords = inputText.split(' ');
-
-        for (const word of splitWords) {
-            const syllables = hyphenator(word, { hyphen: Separator }).split(Separator);
-            for (let i = 0; i < syllables.length; i++) {
-                // If the syllable is a punctuation mark, make it blue
-                if (/[.,!?;:]/.test(syllables[i])) {
-                    output += `<span style="color:${selectedColors.blue}">${syllables[i]}</span>`;
-                } else {
-                    const color = colorToggle ? selectedColors.blue : selectedColors.red;
-                    output += `<span style="color:${color}">${syllables[i]}</span>`;
-                    // Toggle the color if there's another syllable after the current one
-                    if (i < syllables.length - 1) {
-                        colorToggle = !colorToggle;
-                    }
-                }
-            }
-
-            // Reset color toggle to always start the next word with blue
-            colorToggle = true;
-            // output += ' ';  // Add a space after each word
-        }
-
-        document.getElementById('output').innerHTML = output.trim();  // Remove trailing space
-    });
-}
-
+var Separator = "•";
 
 function splitAndDisplay() {
     const inputText = document.getElementById('inputText').value;
@@ -97,28 +30,18 @@ function splitAndDisplay() {
         for (const word of splitWords) {
             const syllables = word.split('-');
             for (let i = 0; i < syllables.length; i++) {
-                // If the syllable is a punctuation mark, make it blue
                 if (/[.,!?;:]/.test(syllables[i])) {
                     output += `<span class="blue">${syllables[i]}</span>`;
                 } else {
                     const colorClass = colorToggle ? 'blue' : 'red';
                     output += `<span class="${colorClass}">${syllables[i]}</span>`;
-                    // Toggle the color if there's another syllable after the current one
                     if (i < syllables.length - 1) {
                         colorToggle = !colorToggle;
                     }
                 }
             }
-
-            // Reset color toggle to always start the next word with blue
-            colorToggle = true;
-
-            output += ' ';
+            colorToggle = true;  // Reset color toggle to always start the next word with blue
         }
-
-        document.getElementById('output').innerHTML = output;
+        document.getElementById('output').innerHTML = output.trim();
     });
 }
-
-
-
