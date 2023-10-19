03d2a698ac68d058c2302febfb69de60f84fc82e
Lukas Flad <fladlukas@icloud.com>
2023-10-19 17:23:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ Hyphenopoly.config({
     require: {
         "de": "FORCEHYPHENOPOLY"
     },
-    exceptions: {
-        "de": "Deutsch-lern-ende"
-    },
     hyphen: "•",
     minWordLength: 3
 });
@@ -13,36 +10,26 @@ var Separator = "•";
 
 function splitAndDisplay() {
     const inputText = document.getElementById('inputText').value;
-    fetch('/split-text', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ text: inputText })
-    })
-    .then(response => response.json())
-    .then(data => {
-        let output = '';
-        let colorToggle = true;  // Always start with blue
+    let output = '';
+    let colorToggle = true;  // Always start with blue
 
-        const splitWords = data.splitWords;
+    const words = inputText.split(' ');
 
-        for (const word of splitWords) {
-            const syllables = word.split('-');
-            for (let i = 0; i < syllables.length; i++) {
-                if (/[.,!?;:]/.test(syllables[i])) {
-                    output += `<span class="blue">${syllables[i]}</span>`;
-                } else {
-                    const colorClass = colorToggle ? 'blue' : 'red';
-                    output += `<span class="${colorClass}">${syllables[i]}</span>`;
-                    if (i < syllables.length - 1) {
-                        colorToggle = !colorToggle;
-                    }
+    for (const word of words) {
+        const syllables = word.split('•');  // Trennzeichen von Hyphenopoly
+        for (let i = 0; i < syllables.length; i++) {
+            if (/[.,!?;:]/.test(syllables[i])) {
+                output += `<span class="blue">${syllables[i]}</span>`;
+            } else {
+                const colorClass = colorToggle ? 'blue' : 'red';
+                output += `<span class="${colorClass}">${syllables[i]}</span>`;
+                if (i < syllables.length - 1) {
+                    colorToggle = !colorToggle;
                 }
             }
-output += ' ';  // Add a space after each word
-colorToggle = true;  // Reset color toggle to always start the next word with blue
         }
-        document.getElementById('output').innerHTML = output.trim();
-    });
+        output += ' ';  // Add a space after each word
+        colorToggle = true;  // Reset color toggle to always start the next word with blue
+    }
+    document.getElementById('output').innerHTML = output.trim();
 }
